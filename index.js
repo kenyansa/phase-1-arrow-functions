eb210c46969c96903deab8b921ae8766556a3d5e
Kenyansa <kenyansajustine@gmail.com>
2022-04-12 14:23:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+// has a function expression called divide:
+// divide divides 2000 by 100:
+let divide = () => 2000 / 100;
+// has an arrow function called square:
+// square arrow function takes one parameter and multiplies it times itself:
+let square = (mult) => mult * mult;
+// has an arrow function called add:
+// add arrow function takes two parameters and adds them together:
+let add = (num1, num2) => num1 + num2;
+
+   
+       
